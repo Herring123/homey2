d077059d8320e391aa184a0a795a46c211d730a1
Ben Herring <ben_herring@hotmail.com>
2023-03-07 19:25:23 +0000

    test push

---
+++ app/controllers/homeys_controller.rb
@@ -0,0 +1,28 @@
+class HomeysController < ApplicationController
+
+# before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def show
+    @homey = Homey.find(params[:id])
+    @comment = Comment.new
+  end
+
+  def update
+    homey = Homey.find(params[:id])
+    if homey.update!(homey_params)
+      flash[:notice] = "Successfully Updated"
+    else
+      flash[:alert] = "Failed to update"
+    end
+    redirect_to homey_path(homey)
+  end
+
+  def homey_params
+    params.require(:homey).permit(:status)
+  end
+end
+
